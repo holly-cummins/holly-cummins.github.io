c8f1a2e2449015a96256a898c66a060d33d7068c
Holly Cummins <cumminsh@uk.ibm.com>
2021-11-16 21:34:10 +0000

    Use description as bio

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: "Holly Cummins", // <title>
   shortSiteTitle: "Holly Cummins", // <title> ending for posts and pages
-  siteDescription: "Holly's personal blog. I'm an IBMer, but this blog isn't speaking for IBM.",
+  siteDescription: "IBMer, Java Champion, occasional maker and regular speaker.",
   siteUrl: "http://hollycummins.com",
   pathPrefix: "",
   siteImage: "preview.png",
