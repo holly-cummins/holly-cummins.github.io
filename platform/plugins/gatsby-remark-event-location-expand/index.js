7d7b825ebe0ab28f3cb31ff54080a84a50652927
Holly Cummins <hcummins@redhat.com>
2022-05-13 07:44:15 +0100

    Reverse changes for experiment to get back in line with upstream

--- platform/plugins/gatsby-remark-event-location-expand/index.js
+++ platform/plugins/gatsby-remark-event-location-expand/index.js
@@ -15,23 +15,19 @@ exports.mutateSource = async ({ markdownNode }) => {
   const { location } = frontmatter;
 
   if (location) {
-    try {
-      const results = await geocoder.geocode(location);
-      // if (results && results.length > 0) {
-      //   // Assume it puts the most likely first
-      //   const res = results[0];
-      //   const geography = {};
-      //   geography.country = res.country;
-      //   geography.countryCode = res.countryCode;
-      //   const flag = flags[res.countryCode];
-      //   if (flag) {
-      //     const buff = Buffer.from(flag);
-      //     geography.flag = buff.toString("base64");
-      //   }
-      //   frontmatter.geography = geography;
-      // }
-    } catch (e) {
-      console.log("Error fetching geography information for ", location);
-    }
+    const results = await geocoder.geocode(location);
+    // if (results && results.length > 0) {
+    //   // Assume it puts the most likely first
+    //   const res = results[0];
+    //   const geography = {};
+    //   geography.country = res.country;
+    //   geography.countryCode = res.countryCode;
+    //   const flag = flags[res.countryCode];
+    //   if (flag) {
+    //     const buff = Buffer.from(flag);
+    //     geography.flag = buff.toString("base64");
+    //   }
+    //   frontmatter.geography = geography;
+    // }
   }
 };
