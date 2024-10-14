f27da27a5cd7b6f10d972eb805c54aa3a0c2ab4e
Holly Cummins <hcummins@redhat.com>
2024-10-14 16:20:44 +0100

    Try setting user agent to see if it makes vimeo not return 401

--- platform/test-integration/links.test.js
+++ platform/test-integration/links.test.js
@@ -1,21 +1,21 @@
-jest.setTimeout(5 * 60 * 1000);
+jest.setTimeout(5 * 60 * 1000)
 
-const link = require("linkinator");
-const status = require("http-status");
-const { curly } = require("node-libcurl");
-const promiseRetry = require("promise-retry");
+const link = require("linkinator")
+const status = require("http-status")
+const {curly} = require("node-libcurl")
+const promiseRetry = require("promise-retry")
 
-const configger = require("../src/utils/configger");
+const configger = require("../src/utils/configger")
 
 describe("site links", () => {
-  const deadExternalLinks = [];
-  const deadInternalLinks = [];
+  const deadExternalLinks = []
+  const deadInternalLinks = []
 
   beforeAll(async () => {
-    const path = "http://localhost:9000";
+    const path = "http://localhost:9000"
 
     // create a new `LinkChecker` that we'll use to run the scan.
-    const checker = new link.LinkChecker();
+    const checker = new link.LinkChecker()
 
     // After a page is scanned, check out the results!
     checker.on("link", async result => {
@@ -23,37 +23,37 @@ describe("site links", () => {
         // Don't stress about 403s from vimeo because humans can get past the paywall fairly easily and we want to have the link
         const isPaywalled =
           result.status === status.FORBIDDEN &&
-          (result.url.includes("vimeo") || result.url.includes("inc.com"));
+          (result.url.includes("vimeo") || result.url.includes("inc.com"))
 
-        let retryWorked;
+        let retryWorked
         if (result.url.includes("twitter")) {
           // Twitter gives 404s, I think if it feels bombarded, so let's try a retry
-          retryWorked = await retryUrl(result.url);
+          retryWorked = await retryUrl(result.url)
         }
 
         // Some APIs give 429s but no retry-after header, and linkinator will not retry in that case
         // Retry the hard way
         if (!retryWorked && result.status === status.TOO_MANY_REQUESTS) {
-          retryWorked = await retryUrl(result.url);
+          retryWorked = await retryUrl(result.url)
         }
 
         if (!retryWorked && !isPaywalled) {
-          const errorText = result.failureDetails[0].statusText || result.failureDetails[0].code;
-          const description = `${result.url} => ${result.status} (${errorText}) on ${result.parent}`;
+          const errorText = result.failureDetails[0].statusText || result.failureDetails[0].code
+          const description = `${result.url} => ${result.status} (${errorText}) on ${result.parent}`
           if (result.url.includes(path)) {
             // This will still miss links where the platform uses the configured url to make it an absolute path, but hopefully we don't care
             // too much about the categorisation as long as *a* break happens
             if (!deadInternalLinks.includes(description)) {
-              deadInternalLinks.push(description);
+              deadInternalLinks.push(description)
             }
           } else {
             if (!deadExternalLinks.includes(description)) {
-              deadExternalLinks.push(description);
+              deadExternalLinks.push(description)
             }
           }
         }
       }
-    });
+    })
 
     const linksToSkip = [
       "https://twitter.com/ducky_devine",
@@ -74,7 +74,7 @@ describe("site links", () => {
       "https://headrush.typepad.com/creating_passionate_users/2006/12/tech_tshirts_ar.html",
       "https://labs.openai.com/", // Blocks scrapers, ironically
       "https://substack.com/" // Blocks scrapers
-    ]; // We know these links are good, and we want to not hit the rate limiters since they appear everywhere
+    ] // We know these links are good, and we want to not hit the rate limiters since they appear everywhere
     // NOTE: The Manning, Medium, and GitHub D is For Duck is fictitious by design, so exclude them
     // DO NOT search and replace these with your own name
 
@@ -91,29 +91,30 @@ describe("site links", () => {
       ],
       retry: true, // Retry on 429 (this only 'applies' if there is also a retry-after header)
       concurrency: 100, // The twitter URLs seem to work better with a high concurrency, counter-intuitively
-      timeout: 30 * 1000
-    });
-  });
+      timeout: 30 * 1000,
+      userAgent: "Linkinator link validation"
+    })
+  })
 
   it("internal links should all resolve", async () => {
-    expect(deadInternalLinks).toEqual([]);
-  });
+    expect(deadInternalLinks).toEqual([])
+  })
 
   it("external links should all resolve", async () => {
-    expect(deadExternalLinks).toEqual([]);
-  });
-});
+    expect(deadExternalLinks).toEqual([])
+  })
+})
 
 const retryUrl = async url => {
   const hitUrl = async retry => {
     // Use a different client, which seems less affected by the 404s from twitter
-    const { statusCode } = await curly.get(url);
+    const {statusCode} = await curly.get(url)
 
     if (status[`${statusCode}_CLASS`] !== status.classes.SUCCESSFUL) {
-      return retry(statusCode);
+      return retry(statusCode)
     }
-  };
-  return promiseRetry(hitUrl, { retries: 4, minTimeout: 61 * 1000 })
+  }
+  return promiseRetry(hitUrl, {retries: 4, minTimeout: 61 * 1000})
     .then(() => true)
-    .catch(() => false);
-};
+    .catch(() => false)
+}
