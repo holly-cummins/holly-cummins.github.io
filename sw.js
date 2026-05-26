5b7bfce8706ecbd4e1f7491cf2c469800979ef91
holly-cummins <holly.cummins@ibm.com>
2026-05-26 16:06:09 +0000

    Deploying to pages from @ holly-cummins/holly-cummins.github.io@40f4fc98ae41349b47daa252336758f997fb8618 🚀

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9d69994588c4a79dfb7c374fe7ae9430"
+    "revision": "b5457f84de588b6a888e2f80cd3d3c9a"
   },
   {
     "url": "manifest.webmanifest",
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-62ebaf8afe2945a3abd3.js`))) {
+  if (!resources || !(await caches.match(`/app-2d515406fd83d3941f17.js`))) {
     return await fetch(event.request)
   }
 
